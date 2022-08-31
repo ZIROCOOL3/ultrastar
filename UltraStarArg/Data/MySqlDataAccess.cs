975de153546afe160cef6cb2489192cdd9cef6be
zirocool2020 <zirocool2003@hotmail.com>
2022-08-31 15:59:47 -0300

    31-08-22
    base

---
+++ UltraStarArg/Data/MySqlDataAccess.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace UltraStarArg.Data
+{
+    public class MySqlDataAccess : IMySqlDataAccess
+    {
+        public async Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                var rows = await connection.QueryAsync<T>(sql, parameters);
+                return rows.ToList();
+            }
+        }
+        public Task SaveData<T>(string sql, T parameters, string connectionString)
+        {
+            using (IDbConnection connection = new MySqlConnection(connectionString))
+            {
+                return connection.ExecuteAsync(sql, parameters);
+            }
+        }
+    }
+}
---
+++ UltraStarArg/Models/Canciones.cs
@@ -0,0 +1,16 @@
+﻿namespace UltraStarArg.Models
+{
+    public class Canciones
+    {
+        public int IdCanciones { get; set; }
+        public int Fk_ideoma { get; set; }
+        public int Fk_artista { get; set; }
+        public int Fk_ritmo { get; set; }
+        public int Año { get; set; }
+        public string Nombre { get; set; }
+        public int Duo { get; set; }
+        public string Imagen { get; set; }
+        public string RutaCancion { get; set; }
+    }
+}
+
