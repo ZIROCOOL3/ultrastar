d30ef13b1473d46e2ec1fd345646c4905153ac7d
zirocool2020 <zirocool2003@hotmail.com>
2022-09-01 10:30:28 -0300

    01-09-22
    version base final

--- UltraStarArg/Models/Canciones.cs
+++ UltraStarArg/Models/Canciones.cs
@@ -2,13 +2,13 @@
 {
     public class Canciones
     {
-        public int IdCanciones { get; set; }
-        public int Fk_ideoma { get; set; }
-        public int Fk_artista { get; set; }
-        public int Fk_ritmo { get; set; }
+        public int Id { get; set; }
+        public string Ideoma { get; set; }
+        public string Artista { get; set; }
+        public string Ritmo { get; set; }
         public int Año { get; set; }
         public string Nombre { get; set; }
-        public int Duo { get; set; }
+        public string Duo { get; set; }
         public string Imagen { get; set; }
         public string RutaCancion { get; set; }
     }
